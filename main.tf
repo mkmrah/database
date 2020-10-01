bc2fcf65d8e6c22560be0648566e2cf3728eaa61
mkmrah <39188713+mkmrah@users.noreply.github.com>
2020-10-01 17:40:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "random_string" "name" {
-  length  = 16
+  length  = 8
   upper   = false
   lower   = true
   number  = false
@@ -7,7 +7,7 @@ resource "random_string" "name" {
 }
 
 resource "ibm_database" "database" {
-  name              = "ibmdb$random_string.result"
+  name              = "$random_string.result"
   plan              = "standard"
   location          = "us-south"
   service           = "databases-for-etcd"
