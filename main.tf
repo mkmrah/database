444fb436fd736bb31b1b92ce61353fd9dae64cdf
mkmrah <39188713+mkmrah@users.noreply.github.com>
2020-10-01 17:22:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,10 @@
-resource "ibm_database" "database" {
-  name              = "mq1-test"
-  plan              = "standard"
-  location          = "us-south"
- // resource_group_id = var.rg_id"
-
-
-  tags = "${concat(var.database_tags, list("GIT_VERSION:1.0"))}"
-  service = "messages-for-rabbitmq"
-  version = "3.7"
-
-  service_endpoints = "public-and-private"
-  members_memory_allocation_mb = 3072
-  members_disk_allocation_mb   = 3072
-
+resource "random_id" "name" {
+  byte_length = 8
 }
 
-variable "database_tags" {
-type = "list"
-default = ["a:b", "c:d"]
+resource "ibm_database" "database" {
+  name              = "ibm-db-$random_id.name.hex"
+  plan              = "standard"
+  location          = "us-south"
+  service           = "databases-for-etcd"
 }
